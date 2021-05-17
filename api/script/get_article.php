a3776a1247409724c153a847352069a250b5d5cc
Marcaaa92 <78621850+Marcaaa92@users.noreply.github.com>
2021-05-17 10:30:41 +0100

    Fixing rewrite, fixing all team filter

--- api/script/get_article.php
+++ api/script/get_article.php
@@ -15,7 +15,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         if ($stmt->rowCount() != 0) {
           if(isset($_GET["category"])){
             $category=$_GET["category"];
-            if($category==""){
+          if($category==""){
               $errore=array("errore"=>"Error missing article type","code"=>400);
               echo json_encode($errore);
               http_response_code(400);
@@ -79,23 +79,66 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
       }
     }
     else{
-        $stmt = $db->prepare("SELECT articles.id, articles.approvated, articles.limited, articles.title, articles.article, articles.subtitle, articles.date, articles.imgdir, user.nickname, category.categoryName, articles.imgdir, team.teamName FROM articles JOIN user ON user.id=articles.journalist JOIN category ON category.id=articles.category JOIN team ON team.id=articles.team WHERE articles.approvated=1 ORDER BY articles.date DESC LIMIT 50");
-        $stmt->execute([]);
-        $rowos=array();
-        while($row = $stmt->fetch()){
-          $rowo=array(
-          "Title"=>$row["title"],
-          "Subtitle"=>$row["subtitle"],
-          "Article"=>$row["article"],
-          "Date"=>$row["date"],
-          "Imgdir"=>"https://blogyouropinion.ddns.net/".$row["imgdir"],
-          "CategoryName"=>$row["categoryName"],
-          "TeamName"=>$row["teamName"],
-          );
-          array_push($rowos,$rowo);
+      if(isset($_GET["category"])){
+        $category=$_GET["category"];
+        if($category==""){
+          $errore=array("errore"=>"Error missing article type","code"=>400);
+          echo json_encode($errore);
+          http_response_code(400);
         }
-        http_response_code(200);
-        echo json_encode($rowos);
+        else if($category!="all"){
+          $stmt = $db->prepare("SELECT categoryName FROM category WHERE id = ?");
+          $stmt->execute([$category]);
+          if ($stmt->rowCount() != 0) {
+            $stmt = $db->prepare("SELECT articles.id, articles.approvated, articles.limited, articles.title, articles.article, articles.subtitle, articles.date, articles.imgdir, user.nickname, category.categoryName, articles.imgdir, team.teamName FROM articles JOIN user ON user.id=articles.journalist JOIN category ON category.id=articles.category JOIN team ON team.id=articles.team WHERE category.id=? AND articles.approvated=1 ORDER BY articles.date DESC LIMIT 50");
+            $stmt->execute([$category]);
+            $rowos=array();
+            while($row = $stmt->fetch()){
+              $rowo=array(
+              "Title"=>$row["title"],
+              "Subtitle"=>$row["subtitle"],
+              "Article"=>$row["article"],
+              "Date"=>$row["date"],
+              "Team"=>$row["teamName"],
+              "Imgdir"=>"https://blogyouropinion.ddns.net/".$row["imgdir"],
+              "Category name"=>$row["categoryName"],
+              );
+              array_push($rowos,$rowo);
+            }
+            http_response_code(200);
+            echo json_encode($rowos);
+          }
+          else{
+            $errore=array("errore"=>"category not found","code"=>404);
+            echo json_encode($errore);
+            http_response_code(404);
+          }
+        }
+        else{
+          $stmt = $db->prepare("SELECT articles.id, articles.approvated, articles.limited, articles.title, articles.article, articles.subtitle, articles.date, articles.imgdir, user.nickname, category.categoryName, articles.imgdir,team.teamName FROM articles JOIN user ON user.id=articles.journalist JOIN category ON category.id=articles.category JOIN team ON team.id=articles.team WHERE articles.approvated=1 ORDER BY articles.date DESC LIMIT 50");
+          $stmt->execute([]);
+          $rowos=array();
+          while($row = $stmt->fetch()){
+            $rowo=array(
+            "Title"=>$row["title"],
+            "Subtitle"=>$row["subtitle"],
+            "Article"=>$row["article"],
+            "Date"=>$row["date"],
+            "Team"=>$row["teamName"],
+            "Imgdir"=>"https://blogyouropinion.ddns.net/".$row["imgdir"],
+            "Category name"=>$row["categoryName"],
+            );
+            array_push($rowos,$rowo);
+          }
+          http_response_code(200);
+          echo json_encode($rowos);
+        }
+      }
+      else{
+        $errore=array("errore"=>"Error missing team name and article type","code"=>400);
+        echo json_encode($errore);
+        http_response_code(400);
+      }
     }
   }
   else{
