ad0073d1c3adb1e61b57d5401c39c346f4717e2d
Marcaaa92 <78621850+Marcaaa92@users.noreply.github.com>
2021-05-22 12:31:23 +0100

    adding team id and article id

--- api/script/get_article.php
+++ api/script/get_article.php
@@ -24,7 +24,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
               $stmt = $db->prepare("SELECT categoryName FROM category WHERE id = ?");
               $stmt->execute([$category]);
               if ($stmt->rowCount() != 0) {
-                $stmt = $db->prepare("SELECT articles.id, articles.approvated, articles.limited, articles.title, articles.article, articles.subtitle, articles.date, articles.imgdir, user.nickname, category.categoryName, articles.imgdir FROM articles JOIN user ON user.id=articles.journalist JOIN category ON category.id=articles.category JOIN team ON team.id=articles.team WHERE team.id=? AND category.id=? AND articles.approvated=1 ORDER BY articles.date DESC LIMIT 50");
+                $stmt = $db->prepare("SELECT articles.id, articles.approvated, articles.limited, articles.title, articles.article, articles.subtitle, articles.date, articles.imgdir, user.nickname, category.categoryName, articles.imgdir,team.teamName, team.id as teamId, category.id AS categoryId FROM articles JOIN user ON user.id=articles.journalist JOIN category ON category.id=articles.category JOIN team ON team.id=articles.team WHERE team.id=? AND category.id=? AND articles.approvated=1 ORDER BY articles.date DESC LIMIT 50");
                 $stmt->execute([$team, $category]);
                 $rowos=array();
                 while($row = $stmt->fetch()){
@@ -33,8 +33,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
                   "Subtitle"=>$row["subtitle"],
                   "Article"=>$row["article"],
                   "Date"=>$row["date"],
+                  "TeamName"=>$row["teamName"],
+                  "TeamID"=>$row["teamId"],
                   "Imgdir"=>"https://blogyouropinion.ddns.net/".$row["imgdir"],
-                  "Category name"=>$row["categoryName"],
+                  "CategoryName"=>$row["categoryName"],
+                  "CategoryId"=>$row["categoryId"],
                   );
                   array_push($rowos,$rowo);
                 }
@@ -48,7 +51,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
               }
             }
             else{
-              $stmt = $db->prepare("SELECT articles.id, articles.approvated, articles.limited, articles.title, articles.article, articles.subtitle, articles.date, articles.imgdir, user.nickname, category.categoryName, articles.imgdir FROM articles JOIN user ON user.id=articles.journalist JOIN category ON category.id=articles.category JOIN team ON team.id=articles.team WHERE team.id=? AND articles.approvated=1 ORDER BY articles.date DESC LIMIT 50");
+              $stmt = $db->prepare("SELECT articles.id, articles.approvated, articles.limited, articles.title, articles.article, articles.subtitle, articles.date, articles.imgdir, user.nickname, category.categoryName, articles.imgdir,team.teamName, team.id AS teamId , category.id AS categoryId FROM articles JOIN user ON user.id=articles.journalist JOIN category ON category.id=articles.category JOIN team ON team.id=articles.team WHERE team.id=? AND articles.approvated=1 ORDER BY articles.date DESC LIMIT 50");
               $stmt->execute([$team]);
               $rowos=array();
               while($row = $stmt->fetch()){
@@ -57,8 +60,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
                 "Subtitle"=>$row["subtitle"],
                 "Article"=>$row["article"],
                 "Date"=>$row["date"],
+                "TeamName"=>$row["teamName"],
+                "TeamID"=>$row["teamId"],
                 "Imgdir"=>"https://blogyouropinion.ddns.net/".$row["imgdir"],
-                "Category name"=>$row["categoryName"],
+                "CategoryName"=>$row["categoryName"],
+                "CategoryId"=>$row["categoryId"],
                 );
                 array_push($rowos,$rowo);
               }
@@ -90,7 +96,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
           $stmt = $db->prepare("SELECT categoryName FROM category WHERE id = ?");
           $stmt->execute([$category]);
           if ($stmt->rowCount() != 0) {
-            $stmt = $db->prepare("SELECT articles.id, articles.approvated, articles.limited, articles.title, articles.article, articles.subtitle, articles.date, articles.imgdir, user.nickname, category.categoryName, articles.imgdir, team.teamName FROM articles JOIN user ON user.id=articles.journalist JOIN category ON category.id=articles.category JOIN team ON team.id=articles.team WHERE category.id=? AND articles.approvated=1 ORDER BY articles.date DESC LIMIT 50");
+            $stmt = $db->prepare("SELECT articles.id, articles.approvated, articles.limited, articles.title, articles.article, articles.subtitle, articles.date, articles.imgdir, user.nickname, category.categoryName, articles.imgdir, team.teamName, team.id as teamId, category.id AS categoryId FROM articles JOIN user ON user.id=articles.journalist JOIN category ON category.id=articles.category JOIN team ON team.id=articles.team WHERE category.id=? AND articles.approvated=1 ORDER BY articles.date DESC LIMIT 50");
             $stmt->execute([$category]);
             $rowos=array();
             while($row = $stmt->fetch()){
@@ -99,9 +105,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
               "Subtitle"=>$row["subtitle"],
               "Article"=>$row["article"],
               "Date"=>$row["date"],
-              "Team"=>$row["teamName"],
+              "TeamName"=>$row["teamName"],
+              "TeamID"=>$row["teamId"],
               "Imgdir"=>"https://blogyouropinion.ddns.net/".$row["imgdir"],
-              "Category name"=>$row["categoryName"],
+              "CategoryName"=>$row["categoryName"],
+              "CategoryId"=>$row["categoryId"],
               );
               array_push($rowos,$rowo);
             }
@@ -115,7 +123,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
           }
         }
         else{
-          $stmt = $db->prepare("SELECT articles.id, articles.approvated, articles.limited, articles.title, articles.article, articles.subtitle, articles.date, articles.imgdir, user.nickname, category.categoryName, articles.imgdir,team.teamName FROM articles JOIN user ON user.id=articles.journalist JOIN category ON category.id=articles.category JOIN team ON team.id=articles.team WHERE articles.approvated=1 ORDER BY articles.date DESC LIMIT 50");
+          $stmt = $db->prepare("SELECT articles.id, articles.approvated, articles.limited, articles.title, articles.article, articles.subtitle, articles.date, articles.imgdir, user.nickname, category.categoryName, articles.imgdir,team.teamName, team.id as teamId, category.id AS categoryId FROM articles JOIN user ON user.id=articles.journalist JOIN category ON category.id=articles.category JOIN team ON team.id=articles.team WHERE articles.approvated=1 ORDER BY articles.date DESC LIMIT 50");
           $stmt->execute([]);
           $rowos=array();
           while($row = $stmt->fetch()){
@@ -124,9 +132,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
             "Subtitle"=>$row["subtitle"],
             "Article"=>$row["article"],
             "Date"=>$row["date"],
-            "Team"=>$row["teamName"],
+            "TeamName"=>$row["teamName"],
+            "TeamID"=>$row["teamId"],
             "Imgdir"=>"https://blogyouropinion.ddns.net/".$row["imgdir"],
-            "Category name"=>$row["categoryName"],
+            "CategoryName"=>$row["categoryName"],
+            "CategoryId"=>$row["categoryId"],
             );
             array_push($rowos,$rowo);
           }
